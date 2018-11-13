b4456b51b4e7f9ea4ac060a62cbfda4e55aca4c2
Иван Загайнов <fohunoff@gmail.com>
2018-11-13 18:02:44 +0300

    Fix news slider on main page

--- src/js/news-slide.js
+++ src/js/news-slide.js
@@ -2,35 +2,31 @@
 
 (function () {
   var news = document.querySelector('.actual-news');
-
-  // Добавляем класс активной новости к первой новости в списке
-  var actualNews = news.querySelector('.actual-news__content');
-  actualNews.classList.add('actual-news__content--active');
-
+  
   if (news !== null) {
+    // Добавляем класс активной новости к первой новости в списке
+    var actualNews = news.querySelector('.actual-news__content');
+    actualNews.classList.add('actual-news__content--active');
+    
     var actualNews = news.querySelectorAll('.actual-news__content');
     var count = 0;
     var slideInterval = setInterval(nextSlide, 5000);
-  
+    
     var bannerImage = document.createElement('img');
     bannerImage.classList.add('actual-news__banner');
     bannerImage.alt = "Текст";
     bannerImage.src = actualNews[0].querySelector('img').src; // Первая картинка
-  
+    
     news.appendChild(bannerImage);
-  
+    
     function nextSlide() {
       actualNews[count].classList.remove('actual-news__content--active');
       count++;                                // Переключаем на следующий слайд
       count = count % actualNews.length;      // Проверяем на остаток
-  
+      
       // ссылка на фото из новости
       bannerImage.src = actualNews[count].querySelector('img').src;
       actualNews[count].classList.add('actual-news__content--active');
     };
   }
-
-  
-
-
 })();
